c2cd9ff88cb512b8b605aaa8fd97b695275258ca
Spencer Salazar <spencer.salazar@gmail.com>
2014-08-12 19:33:00 -0700

    added file

---
+++ OpenGL/chuck_opengl.h
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------
+  chugl
+  Chuck OpenGL library module
+
+  Copyright (c) 2014 Spencer Salazar.  All rights reserved.
+  https://ccrma.stanford.edu/~spencer/
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  U.S.A.
+-----------------------------------------------------------------------------*/
+
+#ifndef CHUCK_OPENGL_H
+#define CHUCK_OPENGL_H
+
+#include "chuck_def.h"
+
+extern t_CKINT Chuck_OpenGL_offset_chugl;
+extern t_CKBOOL OpenGL_query(Chuck_DL_Query *QUERY);
+
+#endif // CHUCK_OPENGL_H
